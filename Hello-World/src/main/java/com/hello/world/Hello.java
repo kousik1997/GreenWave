48dc055b29f725121f3fd1426d541471e9b0d1bc
kousik1997 <93372832+kousik1997@users.noreply.github.com>
2022-06-06 11:02:59 +0530

    Add files via upload

---
+++ Hello-World/src/main/java/com/hello/world/Hello.java
@@ -0,0 +1,42 @@
+package com.hello.world;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class Hello {
+
+	private String name;
+    private int id;
+    private String department;
+    
+    
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+	public void show() {
+		System.out.println("Employee Name - "+ name +", Employee id - "+id+", Employee Department - "+department);
+	}
+    
+    
+}
